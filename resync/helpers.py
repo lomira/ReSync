fbf9ae83cc41d609f3db97c34c0dfd6e16601724
JeanRemi Telmon <jeanremi.telmon@free.fr>
2020-10-01 09:52:15 +0200

    Working save

--- resync/helpers.py
+++ resync/helpers.py
@@ -1,11 +1,28 @@
 import os
 
 
+class FileClass:
+    def __init__(self, path, filename, timestamp):
+        self.path = path
+        self.filename = filename
+        self.timestam = timestamp
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.filename == other.filename and self.timestam == other.timestam
+        else:
+            return False
+
+    def __hash__(self):
+        return hash([self.filename, self.timestam])
+
+
 def list_file(path):
     # r=root, d=directories, f = files
     files = []
     for r, d, f in os.walk(path):
         for file in f:
-            path = os.path.join(r, file)
-            files.append([path, os.path.getmtime(path)])
+            if not file.startswith("~$"):
+                path = os.path.join(r, file)
+                files.append(FileClass(path, file, os.path.getmtime(path)))
     return files
--- resync/resync.py
+++ resync/resync.py
@@ -1,4 +1,6 @@
 import helpers
+import time
+
 
 path_local = "C:\\Users\\jtelmon\\Desktop\\Commercial_JR\\"
 path_distant = "O:\\25 - Microeconomix\\Prospection Commerciale\\Propositions commerciales\\Equipe Afrique\\3) EVAL D'IMPACT\\CV_Ref_Plaquette\\"
@@ -6,6 +8,18 @@ path_distant = "O:\\25 - Microeconomix\\Prospection Commerciale\\Propositions co
 files_local = helpers.list_file(path_local)
 files_distant = helpers.list_file(path_distant)
 
+fnames_local = [f.filename for f in files_local]
+fnames_distant = [f.filename for f in files_distant]
+
+new_files = files_local.copy()
+updated_files = []
+
 for eloc in files_local:
-    if eloc not in files_distant:
-        print(eloc)
+    for edis in files_distant:
+        if eloc == edis:
+            new_files.remove(eloc)
+        elif eloc.filename == edis.filename and eloc.timestam > edis.timestam:
+            updated_files.append(eloc)
+
+print("Nouveaux : ", "\n \t \t ".join([f.path for f in new_files]))
+print("Recents : ", "\n \t \t ".join([f.path for f in updated_files]))
